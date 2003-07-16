7f8f5f6759bfe4b77df5926fb5ee084b760d64e8
nenolod <nenolod>
2003-07-16 04:24:09 +0000

    *** empty log message ***

--- nenobot07/extras/weather.pl
+++ nenobot07/extras/weather.pl
@@ -35,17 +35,7 @@ sub weather {
 
 	# Now... make something useful of it.
 
-#	if ($weatherobj->{winddirtext} eq "Calm") {
-
-#	$line = "Weather for ".$station.": Conditions - $weatherobj->{conditionstext} with no wind. Temperature is at $weatherobj->{temperature_f} deg F. Barometric Pressure is $weatherobj->{pressure_inhg} inches of mercury.";
-
-#	} else {
-
-#	$line = "Weather for ".$station.": Conditions - $weatherobj->{conditionstext} with winds from the $weatherobj->{winddirtext} at $weatherobj->{windspeedmph} MPH. Temperature is at $weatherobj->{temperature_f} deg F. Barometric Pressure is $weatherobj->{pressure_inhg} inches of mercury.";
-	
-#	}
-
-	$line = "\002[\002Weather - $station\002]\002 Conditions: $weatherobj->{conditionstext} Winds: $weatherobj->{winddirtext} at $weatherobj->{windspeedmph} Temperature: $weatherobj->{temperature_f} Barometer: $weatherobj->{pressure_inhg}.";
+	$line = "\002[\002Weather - $station\002]\002 Conditions: $weatherobj->{conditionstext} .:. Winds: $weatherobj->{winddirtext} at $weatherobj->{windspeedmph} MPH ($weatherobj->{windspeedkts} knots) .:. Temperature: $weatherobj->{temperature_f} Farenheit ($weatherobj->{temperature_c} Celsius) .:. Barometer: $weatherobj->{pressure_inhg}.";
 
 	return $line;
 
